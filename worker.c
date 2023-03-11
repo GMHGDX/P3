c22a055822672b5e25ad2bc7901b88915454889c
GabrielleHieken <gabriellehieken@gmail.com>
2023-03-11 17:18:05 -0600

    debugging time

--- worker.c
+++ worker.c
@@ -85,7 +85,7 @@ int main(int argc, char *argv[]){
     double termTogether = (double)(termTimeS) + (double)(termTimeNano/BILLION);
 
 //testingggggggggggggg
-    double testNano = (1.0)(termTimeNano/BILLION);
+    double testNano = (1.0)*(termTimeNano/BILLION);
 
 
     sysClockS = readFromMem.sec;
